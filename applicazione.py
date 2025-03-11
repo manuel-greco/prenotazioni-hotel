4f1d41805830082c46979fad40d7d2e28d117dcf
manuel <manuelofficial716@gmail.com>
2025-03-11 17:03:01 +0100

    menu migliorato

--- applicazione.py
+++ applicazione.py
@@ -43,28 +43,43 @@ while True:
     print(f'| 1. Visualizza tutte le camere{"|":>22}')
     print(f'| 2. Visualizza camere per tipologia{"|":>17}')
     print(f'| 3. Prenota camera{"|":>34}')
-    print(f'| 5. Crea una nuova camera{"|":>27}')
-    print(f'| 6. Elimina una camera{"|":>30}')
+    print(f'| 4. Crea una nuova camera{"|":>27}')
+    print(f'| 5. Elimina una camera{"|":>30}')
     print(f'+{"-"*51}+')
 
     scelta = int(input("\033[93mSeleziona la tua scelta: \033[0m"))
-
+    os.system('cls')
     if scelta == 0:
         break
 
     elif scelta == 1:
+        print(f'{'+':>10}{"-"*51}+')
+        print(f'{'|':>10}{"Visualizza tutte le camere":^51}|')
+        print(f'{'+':>10}{"-"*51}+')
         l.visualizza_camere(camere)
 
     elif scelta==2:
+        print(f'+{"-"*51}+')
+        print(f'|{"Visualizza camere per tipologia":^51}|')
+        print(f'+{"-"*51}+')
         l.visualizza_tipologia(camere)
 
     elif scelta==3:
+        print(f'{'+':>10}{"-"*51}+')
+        print(f'{'|':>10}{"Prenota camera":^51}|')
+        print(f'{'+':>10}{"-"*51}+')
         l.prenota_camera(camere)
 
     elif scelta == 4:
+        print(f'+{"-"*51}+')
+        print(f'|{"Crea una nuova camera":^51}|')
+        print(f'+{"-"*51}+')
         l.crea_camera(camere)
 
     elif scelta == 5:
+        print(f'+{"-"*51}+')
+        print(f'|{"Elimina una camera":^51}|')
+        print(f'+{"-"*51}+')
         l.elimina_camera(camere)
         
     input("\033[93mPremi un tasto per continuare...\033[0m")
